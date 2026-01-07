b903e6128a14be6d48686a3a132b91e2824e0a5f
QuasarDev <quasardev9@gmail.com>
2026-01-07 07:43:07 +0000

    Draft menu page layout for upcoming JS rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
 import { heroContent } from "./home";
 
-const content = document.getElementById("content");
-content.appendChild(heroContent());
+// const content = document.getElementById("content");
+// content.appendChild(heroContent());
